293e6bbd197dbd2897c37a3f37aea4be44915131
David Mancuso <davidjmancuso@gmail.com>
2015-12-09 22:24:30 -0500

    add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: 'Dave',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Dave',
+// 	// 	age: 25
+// 	// }
+// 	obj.age = 25;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+// Array Example
+var values = [15,37]
+function addGrade (arr) {
+	arr.push(1);
+	debugger;
+	// newArr = [100,20,1];
+}
+
+addGrade(values);
+console.log(values);
