45a741f28d5bb32e3223089f01b12a86d2d4e541
David Mancuso <davidjmancuso@gmail.com>
2015-12-20 21:17:45 -0500

    convert GET todos/:id to use sequelize

--- server.js
+++ server.js
@@ -44,15 +44,24 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo){
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
 	});
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	// if (matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
 });
 
 // POST  /todos
@@ -64,17 +73,6 @@ app.post('/todos', function(req, res) {
 	}, function (e) {
 		res.status(400).json(e);
 	});
-
-	// body.description = body.description.trim();
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.id = todoNextId++;
-	// todos.push(body);
-
-	// res.json(body);
 });
 
 //DELETE /todos/:id
